a7ee105124e588e660e1c9e82893c3a34a38c6f4
16fil <c3033291@ncl.ac.uk>
2025-04-11 17:05:17 +0100

    test 5

--- src/main.java
+++ src/main.java
@@ -6,5 +6,8 @@ public static void main(String[] args) {
     System.out.println("1");
     System.out.println("1");
     System.out.println("1");
+    System.out.println("1");
+    System.out.println("1");
+    System.out.println("1");
 }
 }
