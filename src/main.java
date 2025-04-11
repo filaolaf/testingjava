b39abf3a287fbdc2e27cb565034c2c160b48c729
16fil <c3033291@ncl.ac.uk>
2025-04-11 17:05:24 +0100

    test 6

--- src/main.java
+++ src/main.java
@@ -9,5 +9,9 @@ public static void main(String[] args) {
     System.out.println("1");
     System.out.println("1");
     System.out.println("1");
+    System.out.println("1");
+    System.out.println("1");
+    System.out.println("1");
+
 }
 }
